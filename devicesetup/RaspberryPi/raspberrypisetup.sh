62fc8d6f9173b72fe2df04a7c8aedfb0eb44b2d9
Mikhail P. Ortiz-Lunyov <108703641+mportizlunyov@users.noreply.github.com>
2026-02-15 18:39:54 -0600

    Updated graphics, began raspberrypi setup

---
+++ devicesetup/RaspberryPi/raspberrypisetup.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+# CyberRECon 2026 UW-Madison
+# Developed by Mikhail Ortiz-Lunyov
+# February 2026
+#
+# Setup script to set up network monitoring on LAN
+#  via Monitor Mode and WireShark
+#
+# Will use Monitoring Mode on `wlan0`,
+#  therefore ssh into Pi via `eth0`
+#
+# Confirm your sandbox allows file writing!
+
+# https://www.kali.org/blog/raspberry-pi-wi-fi-glow-up/
+# This script will install the following:
+# - Wireshark (twireshark)
+# - Aircrack-ng
+
+
+echo "Make sure you are connected via eth0!"
+
+
+# Kali Linux on RAspbery Pi does NOT enable ssh by default (add file `ssh`` in boot folder)
+
+# Update packages
+sudo apt update
+sudo apt full-upgrade -y
+# Install `wireshark`, `tshark`, `aircrack-ng`
+sudo apt install wireshark tshark aircrack-ng -y
